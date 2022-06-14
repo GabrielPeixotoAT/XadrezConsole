a26b866d7bf38b91d49d5dd24fbbd7f56c52120e
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-06-14 11:41:56 -0300

    Começando a tratar erros com execao personalizada

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ namespace XadrezConsole
     {
         public static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.SetPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-            tab.SetPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-            tab.SetPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
+                tab.SetPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.SetPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.SetPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
 
-            Tela.ImprimirTabuleiro(tab);
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroExeption e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ tabuleiro/TabuleiroExeption.cs
@@ -0,0 +1,10 @@
+namespace XadrezConsole.tabuleiro
+{
+    class TabuleiroExeption : Exception
+    {
+        public TabuleiroExeption(string msg) : base(msg)
+        {
+            
+        }
+    }
+}
